ee8bfc115c77e855911852f33ad1e8f05571121f
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-14 15:20:04 +0530

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Rishi Singh on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
