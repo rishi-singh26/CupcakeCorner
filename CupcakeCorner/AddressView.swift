dc94de14834ebac092c4520dc00ed330831e6102
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-14 17:56:33 +0530

    Checking for a valid address

--- CupcakeCorner/AddressView.swift
+++ CupcakeCorner/AddressView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 struct AddressView: View {
     @ObservedObject var order: Order
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Checkout")
+                }
+            }
+            .disabled(!order.hasValidAddress)
+        }
+        .navigationTitle("Address Details")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct AddressView_Previews: PreviewProvider {
     static var previews: some View {
-        AddressView(order: Order())
+        NavigationView {
+            AddressView(order: Order())
+        }
     }
 }
