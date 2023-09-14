b48a241cfffca81cb6e88ee4d3d3e98d1f6e4f87
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-14 17:44:48 +0530

    Taking basic order details

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,37 +7,42 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var userName = ""
-    @State private var email = ""
+    @StateObject var order = Order()
     var body: some View {
-        Form {
-            Section {
-                TextField("User Name", text: $userName)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create Accoount") {
-                    print("Creating account")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes \(order.quantity)", value: $order.quantity, in: 3...20)
                 }
-                .disabled(disableForm)
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                            .transition(.move(edge: .top))
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                            .transition(.move(edge: .top))
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
+                }
             }
+            .navigationTitle("Cupcake Corner")
         }
-    }
-    
-    var disableForm: Bool {
-        return email.count < 5 || email.count < 5
     }
 }
 
